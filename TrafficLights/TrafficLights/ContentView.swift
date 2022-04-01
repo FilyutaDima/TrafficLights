58bbba7eb9bc53baafab9fd066379187da138e03
Dmitry <filyuta.dima@yandex.ru>
2022-04-01 10:01:48 +0300

    Project created

---
+++ TrafficLights/TrafficLights/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLights
+//
+//  Created by Dmitry on 1.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLights/TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Dmitry on 1.04.22.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
